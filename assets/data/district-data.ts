06ac1c4fb09bbe5cd405665652cbd440c5ef9cac
Yulia Zherebtsova <juliaz1206@yandex.ru>
2024-09-17 12:40:39 +0300

    Deploy Angular project with correct file structure

---
+++ assets/data/district-data.ts
@@ -0,0 +1,117 @@
+interface DistrictInfo {
+	id: string;
+	districtName: string;
+	population: number;
+	area: number;
+}
+
+export const districtData: DistrictInfo[] = [
+	{
+			id: 'admiralteyskiy',
+			districtName: 'Адмиралтейский район',
+			population: 153735,
+			area: 13.82,
+	},
+	{
+			id: 'vasileostrovskiy',
+			districtName: 'Василеостровский район',
+			population: 202288,
+			area: 14.64,
+	},
+	{
+			id: 'vyborgskiy',
+			districtName: 'Выборгский район',
+			population: 528147,
+			area: 115.38,
+	},
+	{
+			id: 'kalininskiy',
+			districtName: 'Калининский район',
+			population: 512182,
+			area: 40.12,
+	},
+	{
+			id: 'kirovskiy',
+			districtName: 'Кировский район',
+			population: 325118,
+			area: 48,
+	},
+	{
+			id: 'kolpinskiy',
+			districtName: 'Колпинский район',
+			population: 197253,
+			area: 105.8,
+	},
+	{
+			id: 'krasnogvardeyskiy',
+			districtName: 'Красногвардейский район',
+			population: 354989,
+			area: 56.83,
+	},
+	{
+			id: 'krasnoselskiy',
+			districtName: 'Красносельский район',
+			population: 419779,
+			area: 115,
+	},
+	{
+			id: 'kronshtadtskiy',
+			districtName: 'Кронштадтский район',
+			population: 44290,
+			area: 19.35,
+	},
+	{
+			id: 'kurortnyy',
+			districtName: 'Курортный район',
+			population: 79842,
+			area: 267.9177,
+	},
+	{
+			id: 'moskovskiy',
+			districtName: 'Московский район',
+			population: 342562,
+			area: 71.07,
+	},
+	{
+			id: 'nevskiy',
+			districtName: 'Невский район',
+			population: 540415,
+			area: 61.79,
+	},
+	{
+			id: 'petrogradskiy',
+			districtName: 'Петроградский район',
+			population: 124132,
+			area: 24,
+	},
+	{
+			id: 'petrodvortsovyy',
+			districtName: 'Петродворцовый район',
+			population: 142397,
+			area: 109,
+	},
+	{
+			id: 'primorskiy',
+			districtName: 'Приморский район',
+			population: 585554,
+			area: 109.87,
+	},
+	{
+			id: 'pushkinskiy',
+			districtName: 'Пушкинский район',
+			population: 233856,
+			area: 239.95,
+	},
+	{
+			id: 'frunzenskiy',
+			districtName: 'Фрунзенский район',
+			population: 383799,
+			area: 37.469,
+	},
+	{
+			id: 'tsentralnyy',
+			districtName: 'Центральный район',
+			population: 207165,
+			area: 17.12,
+	},
+];
